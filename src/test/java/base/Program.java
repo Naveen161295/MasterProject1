5370e9a2dc84bc11d95aa08af8cf0d323d9c5edd
Naveen161295 <naveen161295@gmail.com>
2021-07-22 11:04:13 +0530

    Method11

--- src/test/java/base/Program.java
+++ src/test/java/base/Program.java
@@ -4,7 +4,7 @@ package base;
 
 public class Program extends BaseClassCreation {
 	public static void master1() {
-		System.out.println("method1");
+		System.out.println("method11");
 	}
 	public static void main(String[] args) {
 		master1();
